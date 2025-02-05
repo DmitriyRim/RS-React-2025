4296047d23c10f6428035627d93a61080bf7d7d1
Rimfis <dimitri.rim@yandex.ru>
2025-02-05 17:11:11 +0300

    refactor: switching from the App class component to functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,22 @@
-import { Component } from 'react';
 import './App.css';
 import Search from './components/Search/Search';
 import CardList from './components/CardList/CardList';
-import ErrorBoundary, {
-  ErrorButton,
-} from './components/ErrorBoundary/ErrorBoundary';
+import { ErrorButton } from './components/ErrorBoundary/ErrorBoundary';
+import { Outlet } from 'react-router-dom';
 
-type State = {
-  searchInput: string;
-  currentQuery: string;
-};
-
-class App extends Component<unknown, State> {
-  constructor(props: unknown) {
-    super(props);
-    this.state = {
-      searchInput: this.initialState(),
-      currentQuery: this.initialState(),
-    };
-  }
-
-  handleChangeInput = (e: { target: { value: string } }) => {
-    const value = e.target.value;
-
-    localStorage.setItem('search', value);
-    this.setState((prevState) => {
-      return { ...prevState, searchInput: value };
-    });
-  };
-
-  handleSearch = () => {
-    this.setState((prevState) => {
-      return { ...prevState, currentQuery: prevState.searchInput.trim() };
-    });
-  };
-
-  initialState = (): string => {
-    let ls = localStorage.getItem('search');
-
-    if (!ls) {
-      ls = '';
-      localStorage.setItem('search', ls);
-    }
-
-    return ls;
-  };
-
-  render() {
-    return (
-      <ErrorBoundary fallback={<p>Something went wrong</p>}>
-        <header>
-          <Search />
-        </header>
-        <main>
-          <CardList />
-        </main>
-        <footer>
-          <ErrorButton />
-        </footer>
-      </ErrorBoundary>
-    );
-  }
+export default function App() {
+  return (
+    <>
+      <header>
+        <Search />
+      </header>
+      <main>
+        <CardList />
+        <Outlet />
+      </main>
+      <footer>
+        <ErrorButton />
+      </footer>
+    </>
+  );
 }
-
-export default App;
