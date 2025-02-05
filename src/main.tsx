07d22c469dc5b4955aed52b8b2f4b0d52ace5555
Rimfis <dimitri.rim@yandex.ru>
2025-02-05 16:34:04 +0300

    feat: add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/:id',
+        element: 'DetailsCard',
+      },
+    ],
+  },
+]);
+
 createRoot(document.getElementById('root') as Element).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary fallback={<p>Something went wrong</p>}>
+      <RouterProvider router={router} />
+    </ErrorBoundary>
   </StrictMode>
 );
