aac2f9b061f9177d01eca56b2d56223cf5b771d7
Rimfis <dimitri.rim@yandex.ru>
2025-02-07 11:06:52 +0300

    fix: add navigate useLocalStorage

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const useLocalStorage = () => {
   const [query, setQuery] = useState('');
+  const navigate = useNavigate();
+
   useEffect(() => {
     let ls = localStorage.getItem('searchQuery');
 
     if (!ls) {
       ls = '';
     }
-
+    navigate(`?search=${ls}`);
     setQuery(ls);
-  }, []);
+  }, [navigate]);
 
   return query;
 };
