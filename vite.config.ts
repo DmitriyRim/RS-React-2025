3201caba9a63006bc15cd067ff50bd098aa7892d
Rimfis <dimitri.rim@yandex.ru>
2025-02-08 17:07:14 +0300

    feat: add vitest, react-test-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+    coverage: {
+      include: ['**/*.tsx'],
+      exclude: [
+        '**/node_modules/**',
+        '**/*.test.tsx',
+        '**/*.spec.tsx',
+        'src/__tests__/setup.ts',
+      ],
+    },
+  },
 });
