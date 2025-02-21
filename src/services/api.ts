1ea3a26175f72cd10706a65d31a5a05b8afadacd
Rimfis <dimitri.rim@yandex.ru>
2025-02-21 10:10:05 +0300

    feat: add test for ThemeButton

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,23 +1,4 @@
-import { Book, ResponseBooks } from '../types/types';
 import { LoaderFunctionArgs } from 'react-router-dom';
-
-const BASE_URL = 'https://gutendex.com/books/';
-
-export async function getData(
-  search: string = ''
-): Promise<ResponseBooks | Book | string> {
-  try {
-    const response = await fetch(BASE_URL + search);
-
-    if (response.ok) {
-      return await response.json();
-    } else {
-      throw new Error(`${response.status}`);
-    }
-  } catch (error) {
-    return new Error(`${error}`).message;
-  }
-}
 
 export async function rootLoader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url);
