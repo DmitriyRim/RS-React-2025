eb54db85d58d220166de5133819493bf3d7fa50a
Rimfis <dimitri.rim@yandex.ru>
2025-03-22 11:25:02 +0300

    feat: add sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllData, getAllRegions } from './utils/utils';
+import {
+  filterData,
+  getAllData,
+  getAllRegions,
+  searchData,
+  sortData,
+} from './utils/utils';
 import Card from './components/Card';
 import { Country } from './types/types';
 
@@ -9,6 +15,8 @@ function App() {
   const [displayData, setDisplayData] = useState<Country[]>([]);
   const [regions, setRegions] = useState<string[]>([]);
   const [currentRegion, setCurrentRegion] = useState<string>('all');
+  const [search, setSearch] = useState<string>('');
+  const [sort, setSort] = useState<string>('default');
 
   useEffect(() => {
     const getData = async () => {
@@ -22,19 +30,16 @@ function App() {
   }, []);
 
   useEffect(() => {
-    const filterData = (data: Country[]) => {
-      let filterData = [...data];
-      if (currentRegion !== 'all') {
-        filterData = filterData.filter((data) => data.region === currentRegion);
-      }
-      setDisplayData(filterData);
-    };
-    filterData(data);
-  }, [currentRegion, data]);
+    let newData = [...data];
+    newData = filterData([...data], currentRegion);
+    newData = searchData(newData, search);
+    newData = sortData(newData, sort);
+    setDisplayData(newData);
+  }, [currentRegion, data, search, sort]);
 
   return (
     <>
-      <h1>performance</h1>
+      <h1>Performance</h1>
       <select
         value={currentRegion}
         onChange={(e) => {
@@ -48,6 +53,15 @@ function App() {
           </option>
         ))}
       </select>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <select value={sort} onChange={(e) => setSort(e.target.value)}>
+        <option value="abs">Population ↑</option>
+        <option value="xyz">Population ↓</option>
+      </select>
       {displayData.map((country) => (
         <Card country={country} key={country.name.common} />
       ))}
@@ -56,9 +70,3 @@ function App() {
 }
 
 export default App;
-/*
-Фильтр: позволяет пользователям фильтровать страны по регионам с помощью выпадающего меню.
-Поиск: Добавьте строку поиска, чтобы пользователи могли выполнять поиск стран по названию.
-Сортировка: добавьте возможность сортировать страны по численности населения или названию
-(по возрастанию/убыванию).
-*/
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -17,3 +17,29 @@ export function getAllRegions(data: Country[]) {
   });
   return regions;
 }
+
+export const filterData = (
+  data: Country[],
+  currentRegion: string
+): Country[] => {
+  if (currentRegion !== 'all') {
+    return data.filter((data) => data.region === currentRegion);
+  }
+  return data;
+};
+
+export const searchData = (data: Country[], search: string): Country[] => {
+  const regex = new RegExp(search, 'i');
+  return data.filter((country) => regex.test(country.name.common));
+};
+
+export const sortData = (data: Country[], sortBy: string): Country[] => {
+  const newData = [...data];
+  if (sortBy === 'abs') {
+    return newData.sort((a, b) => a.population - b.population);
+  } else if (sortBy === 'xyz') {
+    return newData.sort((a, b) => b.population - a.population);
+  } else {
+    return newData;
+  }
+};
